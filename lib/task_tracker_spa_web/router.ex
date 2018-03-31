95c2e530be26f5a1abb9e327ce32947ac57b3e06
glyn <glynnnnnnnnn@gmail.com>
2018-03-31 03:28:30 -0400

    create user & task json && finished create user with hash stored

--- lib/task_tracker_spa_web/router.ex
+++ lib/task_tracker_spa_web/router.ex
@@ -17,6 +17,8 @@ defmodule TaskTrackerSpaWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/users", UserController, except: [:new, :edit]
+    resources "/tasks", TaskController, except: [:new, :edit]
   end
 
   # Other scopes may use custom stacks.
